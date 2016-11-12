05d4ecb67994d90b5d35ac48a0603611cafa0b04
oracle <oracle@GBTC441R07RH.montgomerycollege.edu>
2016-11-12 09:51:01 -0500

    initial commit

---
+++ src/FizzBuzzMain.java
@@ -0,0 +1,26 @@
+
+public class FizzBuzzMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*Write a program that prints the numbers from 1 to 100. 
+ * But for multiples of three print “Fizz” instead of the number 
+ * and for the multiples of five print “Buzz”. 
+ * For numbers which are multiples of both three and five print “FizzBuzz”.*/
+		for (int i = 1; i < 101; i++){
+/*You could also use (i % 15==0)*/
+			if (i % 3==0 && i % 5 ==0){
+				print("FizzBuzz");			
+			}else if(i % 3==0){
+				print("Fizz");
+			}else if(i % 5==0) {
+				print("Buzz");
+			}else{
+				print(Integer.toString(i));
+			}
+		}	
+	}
+	private static void print(String memo){
+		System.out.println(memo);
+	}
+}
